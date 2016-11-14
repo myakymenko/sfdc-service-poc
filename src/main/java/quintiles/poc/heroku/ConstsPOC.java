2e56ce5fbc07e5d0db6b0ae8af37e0fc77f3c3ed
myakymenko <myakymenko@corevalue.net>
2016-11-15 00:03:23 +0200

    update creds

--- src/main/java/quintiles/poc/heroku/ConstsPOC.java
+++ src/main/java/quintiles/poc/heroku/ConstsPOC.java
@@ -5,8 +5,10 @@ public class ConstsPOC {
 	public static final double API_VERSION = 38.0;
 	public static final String URL = "https://login.salesforce.com/services/Soap/c/" + API_VERSION;
 	//static final String URL = "https://eu5.salesforce.com/services/Soap/c/38.0";
-	public static final String USERNAME = "maxim.yakimenko@gmail.com.ceo";
-	public static final String PASSWORD = "max701198838hKFvJJPtHG8n3fnko8ymkKZ";
+	/*public static final String USERNAME = "maxim.yakimenko@gmail.com.ceo";
+	public static final String PASSWORD = "max701198838hKFvJJPtHG8n3fnko8ymkKZ";*/
+	public static final String USERNAME = "myakymenko@ims.poc";
+	public static final String PASSWORD = "max7011988oFZPKABDlsT01u8s1rWoCA0K";
 	
     // one second in milliseconds
 	public static final long ONE_SECOND_MS = 1000;
